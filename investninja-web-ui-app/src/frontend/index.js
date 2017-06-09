78b78eb368831c7d18f4a7b4e31a1afcf1b513ac
AlexandreSabino <alexandre.sabino.avsd@gmail.com>
2017-06-08 21:15:22 -0300

    schema de rotas.

--- investninja-web-ui-app/src/frontend/index.js
+++ investninja-web-ui-app/src/frontend/index.js
@@ -3,17 +3,28 @@ import ReactDOM from 'react-dom';
 import App from './component/App';
 import { AppContainer } from 'react-hot-loader';
 import { overrideComponentTypeChecker } from 'react-toolbox';
+import { Route, BrowserRouter } from 'react-router-dom';
+
 
 const rootEl = document.getElementById('app');
 
 const render = () => {
   ReactDOM.render(
-    <AppContainer>
-      <App />
+    <AppContainer>      
+      <BrowserRouter>
+        <div>
+          <Route exact path='/' component={App} />
+          <Route exact path='/home' component={App} />        
+          <Route exact path='/ninja' component={App} />        
+        </div>
+      </BrowserRouter>
     </AppContainer>,
     rootEl
   );
 };
+
+
+
 
 if (process.env.NODE_ENV !== 'production') {
   overrideComponentTypeChecker((classType, reactElement) => (
