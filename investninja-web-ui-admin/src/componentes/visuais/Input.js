acb458c6bb7b4f7089d780bcf6215ec92d416600
AlexandreSabino <alexandre.sabino.avsd@gmail.com>
2017-06-10 00:50:58 -0300

    Ao infinito e alem.

---
+++ investninja-web-ui-admin/src/componentes/visuais/Input.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export default class Input extends React.Component {
+
+    defineSizeLabel() {
+        let result = Math.min(4, 12 - this.props.columns);
+        return "col-sm-" + result + " control-label";
+    }
+
+    defineSizeInput() {        
+        return "col-sm-" + this.props.columns;
+    }
+
+    render() {
+        return (
+            <div className="form-group">
+                <label className={this.defineSizeLabel()} htmlFor={this.props.name}>{this.props.label}</label>
+                <div className={this.defineSizeInput()}>
+                    <input 
+                        type={this.props.type} 
+                        id={this.props.name} 
+                        name={this.props.name}  
+                        placeholder={this.props.placeholder} 
+                        className="typeahead scrollable tt-input col-sm-12" 
+                        value={this.props.value}
+                        onChange={this.props.onChange}/>                        
+                </div>
+            </div>
+        )
+    }
+}
--- investninja-web-ui-admin/src/index.js
+++ investninja-web-ui-admin/src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import "./assets/css/bootstrap.min.css";
+import "./assets/font-awesome/4.5.0/css/font-awesome.min.css" ;
+import "./assets/css/fonts.googleapis.com.css";
+import "./assets/css/fonts.googleapis.com.css";
+import "./assets/css/ace-skins.min.css";
+import "./assets/css/ace-rtl.min.css";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
